6ad45e7739a4863b02156a39903a4660ffdabf89
Venketaram Ramachandran <v.ramachandran28@berkeley.edu>
2012-04-18 15:47:50 -0700

    merge

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "Positive Suggest Prototype"
+    val appName         = "PositiveSuggest"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
